8edfcee9b54b840b24edbae6daca0c8cc9f4b4ab
DOOmsdAi <vadim.snake96@mail.ru>
2018-11-25 21:22:01 +0000

    Add files via upload

--- raspberry-programm-master/main.py
+++ raspberry-programm-master/main.py
@@ -8,12 +8,17 @@ m = PyMouse()
 m.move(600,600)
 
 oxygen = 60
-OC2 = 20
-Fuel = 100
-batery = 100
+pressure = 59
+humidity = 42 #static
+temperatute = 21 #static
+OC2 = 0
+Fuel = 47 #static
+batery = 47 #static
+voltageSP = 80 #static
 
 CBLUE = '\033[94m'
 CRED = '\033[31m'
+CGREEN = '\033[92m'
 CEND = '\033[0m'
 while 1:
 
@@ -29,17 +34,22 @@ while 1:
                                                               
                                                                             
 	""" + CEND)
-	print (CRED + tabulate([['oxygen', str(oxygen) + "%", '100%'], ['CO2', str(OC2) + "%", '20%'], ['Fuel', str(Fuel) + "%", '100%'], ['batery', str(batery) + "W", '100%']],
+
+	print (CGREEN + tabulate([['Pressure', str(pressure) + ' kPa'], ['Pressure', str(temperatute) + ' C', "Norm"], ['Humidity', str(humidity) + ' %', "Norm"], ['oxygen', str(oxygen) + " %", "Abnormally"], ['CO2', str(OC2) + " %"], ['Voltage of solar panel', str(voltageSP) + " V", "Norm"], ['Batery voltage', str(batery) + " V", "Norm"], ['Fuel', str(Fuel) + " %", "Norm"]],
 	 headers=['Parameter', 'Value', 'Status']) + CEND)
-	if oxygen < 10:
-		print(CRED + "\n \nCritical level of oxygen!" +  CEND)
-	if oxygen == 2:
-		print(CRED + "\n \nYou Are Dead!" +  CEND)
+
+	print(CRED + "\n \nAttention! L'impermeabilite de la navette est cassee!" +  CEND)
+
+	if oxygen <= 10:
+		print(CRED + "\n \nNiveau critique d'oxygène" +  CEND)
+
+	if oxygen == 0:
 		time.sleep(20)
 		break
 		
 	oxygen -= 1
-	OC2 += 1
+	OC2 += 0.5
 	m.click(600,600)
-	time.sleep(60)
+	time.sleep(0.2)
+	# time.sleep(60)
 	os.system('clear')
